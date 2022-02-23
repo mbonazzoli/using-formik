aa2d174a46dd614df429d23b7b22fb181d5559f8
mbonazzoli <mtlb215@gmail.com>
2022-02-22 20:26:16 -0800

    create form and add validation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { useFormik } from 'formik'
 
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    onSubmit: values => {
+      console.log('form: ', values)
+      window.alert('Login Successful')
+    },
+    validate: values => {
+      let errors = {}
+      let emailErrors = validateEmail(values.email)
+      let passwordErrors = validatePassword(values.password)
+      
+      if(emailErrors) errors.email = emailErrors;
+      if(passwordErrors) errors.password = passwordErrors;
+      console.log('errors: ', errors)
+      return errors
+    }
+  });
+  
+  function validateEmail(email) {
+    return validateFieldHasValue(email) || isEmailFormat(email);
+  }
+
+  function validatePassword(password) {
+    return validateFieldHasValue(password)
+  }
+
+  function validateFieldHasValue(value) {
+    return !value ? 'Field required' : undefined;
+  }
+
+  function isEmailFormat(email) {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) ? 
+    undefined : 'Username should be an email'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <form onSubmit={formik.handleSubmit}>
+          <div>Username</div>
+          <input id="emailField" name="email" type="text" onChange={formik.handleChange} value={formik.values.email}/>
+          {formik.errors.email ? 
+            <div id="emailError" style={{color: 'red'}}>{formik.errors.email}</div> : null}
+
+          <div>Password</div>
+          <input id="pswField" name="password" type="text" onChange={formik.handleChange} value={formik.values.password}/>  
+          {formik.errors.password ? 
+            <div id="pswError" style={{color: 'red'}}>{formik.errors.password}</div>: null}
+
+          <button id="submitBtn" type="submit">Submit</button>    
+      </form>
     </div>
   );
 }
